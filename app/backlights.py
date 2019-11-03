24eb44b6db825baabb8eecb08adf784b81319092
Kirill Bazarov <kl.bazarov@gmail.com>
2019-11-04 01:01:31 +0300

    first commit

---
+++ app/backlights.py
@@ -0,0 +1,92 @@
+import sys
+import urllib.request
+from xml.dom.minidom import parseString
+import time
+import psycopg2
+
+#--*-- coding: utf-8 --*--
+import urllib.request
+from xml.dom.minidom import parseString
+
+class ya_search:
+    def __init__(self,query,region='213',page='100'):
+        self.query=query
+        self.region=region
+        self.page=page
+    ya_user='kl.bazarov@gmail.com'
+    ya_key='688c05a0a9c347b99c960a6d5792eea2'
+    
+    #ya_user='reactstudio'
+    #ya_key='03.513174037:5f1f2bd9e6e8f8f93b10e65aa3d147fb'
+    #ya_user='nlp-marketing'
+    #ya_key='03.63209051:ee66402f83ea55f2ba77bb1ad4a1d19c'
+    
+    def yandex_key(self):
+        return ('https://xmlproxy.ru/search/xml?user='+self.ya_user+'&key='+self.ya_key+'&lr=')
+    def xml_build(self):
+        defaults_xml='''<?xml version="1.0" encoding="UTF-8"?>
+            <request>
+            <query>%s</query>
+            <groupings>
+            <groupby attr="d" mode="deep" groups-on-page="%s"  docs-in-group="1" />
+            </groupings>
+            </request>'''
+        return (defaults_xml % (self.query,self.page)).encode()
+    def response(self):
+        ya_url=self.yandex_key()+self.region
+        return (urllib.request.urlopen(ya_url,self.xml_build())).read().decode()
+    def urls(self, url_counts):
+        urls_xml=[]
+        dom_xml=parseString(self.response()).getElementsByTagName('url')
+        for node in dom_xml:
+            urls_xml.append(node.childNodes[0].nodeValue.lower())
+        return urls_xml[:url_counts]
+
+    def parse(self,url):
+        i=0
+        xml_urls=self.urls(10)
+        #print(xml_urls)
+        while i<len(xml_urls):
+            if url in xml_urls[i]:
+                break
+            i += 1
+        if i+1<len(xml_urls):
+            return i+1,xml_urls[i]
+        else:
+            return 0, 'Нет в топ 100'
+    def parse2(self,url):
+        xml_urls=self.urls()
+        results=[x for x in xml_urls if url in x ]
+        try:
+            return (xml_urls.index(results[0])+1,results[0])
+        except:
+            return 0, 'Нет в топ 100'
+
+def go_go(query_lst):
+
+#    test = ya_search()
+#    res = test.parse("https://www.eskimoscow.ru/")
+
+#    domain = 'https://www.eskimoscow.ru/'
+#    project = 'йоооо'
+#    query_lst = ['ремонт холодильников']
+
+#    con = psycopg2.connect(dbname='seohack', user='seohack', 
+#                            password='my_password', host='localhost')
+#    print("Database opened successfully")
+#    cur = con.cursor()
+    res2 = dict()
+#    curr_data = time.strftime('%x')
+    for i in query_lst:
+        test = ya_search(i)
+        res2[i] = test.urls(10)
+#        res = test.parse(domain)
+#        print(res)
+#        res2.append((project, domain, i, res[0], res[1], curr_data))
+#        cur.execute("INSERT INTO seohacks_positions (project, domain, phrase, phrase_position, url_position, data_position) VALUES (%s, %s, %s, %s, %s, %s)", (project, domain, i, res[0], res[1], curr_data))
+#    con.commit()
+#    print("Record inserted successfully")  
+
+#    con.close()
+    print(res2)
+    return res2
---
+++ app/routes.py
@@ -0,0 +1,306 @@
+from app import app
+from flask import render_template, flash, redirect, url_for, request, jsonify
+from app.forms import LoginForm, Top10Form, RegistrationForm
+from app.models import User, Books
+import uuid
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app import db
+from datetime import datetime
+from app.forms import EditProfileForm
+import nltk
+import os
+import requests
+import operator
+import re
+from collections import Counter
+from bs4 import BeautifulSoup
+from .backlights import go_go
+import json
+
+@app.route('/', methods=['GET', 'POST'])
+#@app.route('/index', methods=['GET', 'POST'])
+#@login_required
+def index():
+    title ='Главная'
+    h1 = {'h1': 'Инструменты для SEO-специалистов'}
+    services = [
+    {
+        'title': 'Лемматизация',
+        'description': 'С помощью инструмента можно быстро лемматизировать текст (приводит к именительному падежу единственному числу) и проверять на N-граммы в тексте.'
+    },
+    {
+        'title': 'Парсинг подсветок',
+        'description': 'Инструмент, который поможет по запросу спарсить подсветки (выделенные жирным) поисковой системы Google.'
+    },
+    {
+        'title': 'Выгрузка ТОП-10',
+        'description': 'Инструмент, который поможет быстро выгрузить топ-10 сайтов по заданным запросам, в поисковой системе Google.'
+    },
+    ]
+    return render_template('index.html', title=title, h1=h1, services=services)
+
+#('/top10/', methods=['GET', 'POST'])
+@app.route('/top10/')
+def top10():
+    form = Top10Form()
+   
+    return render_template('top10.html',form=form)
+
+@app.route('/top10/res', methods=['POST', 'GET'])
+def top_10_res():
+    #form = Top10Form()
+    if request.method == "POST":
+        list_of_key = request.form['keys'].split('\n')
+        pos = go_go(list_of_key)
+    #return jsonify(data = {'query': list_of_key})
+    return jsonify(pos)
+    #return render_template('top10.html',jsonify(data=pos))
+
+@app.route('/signUpUser', methods=['POST'])
+def signUpUser():
+    user =  request.form['username'];
+    password = request.form['password'];
+    text = request.form['textik']
+    return jsonify({'status':'OK','user':user,'pass':password,'text':text});
+
+def count_and_save_words(url):
+
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text).get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
+
+stops = [
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
+    'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his',
+    'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
+    'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
+    'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+    'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having',
+    'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if',
+    'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for',
+    'with', 'about', 'against', 'between', 'into', 'through', 'during',
+    'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in',
+    'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then',
+    'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any',
+    'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no',
+    'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's',
+    't', 'can', 'will', 'just', 'don', 'should', 'now', 'id', 'var',
+    'function', 'js', 'd', 'script', '\'script', 'fjs', 'document', 'r',
+    'b', 'g', 'e', '\'s', 'c', 'f', 'h', 'l', 'k'
+]
+
+@app.route('/freq', methods=['GET', 'POST'])
+def freq():
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+
+    return render_template('freq.html', results=results)
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
+    else:
+        return "Nay!", 202
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash(_('Invalid username or password'))
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/profile/<int:id>')
+def profile(id):
+    user = User()
+    return "Hello world {}".format(id)
+
+@app.route('/_add_numbers', methods=['POST'])
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify({'data': render_template('_add_numbers.html', result=a + b)})
+
+#@app.route("/<any(plain, jquery, fetch):js>")
+#def index(js):
+ #   return render_template("{0}.html".format(js), js=js)
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+     return jsonify('pong!')
+BOOKS = [
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
